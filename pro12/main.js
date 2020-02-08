e59245df255e36e81877801313dd673463786f0d
akshaykn <akshaykrishna633@gmail.com>
2020-02-08 21:04:52 +0530

    minor fix

---
+++ pro12/main.js
@@ -0,0 +1,7 @@
+srcUrl = '';
+if(location.href == 'https://akshaykn.github.io/css/pro12') {
+   srcUrl="https://akshaykn.github.io/css/pro12/assets/video/_30 Seconds in the Garden - Ornamental Cut Flower Gardening.mp4";
+} else {
+    srcUrl="assets/video/_30 Seconds in the Garden - Ornamental Cut Flower Gardening.mp4";
+}
+
